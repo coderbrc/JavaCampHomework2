b5a5fd93dbe4265e33107bdf390b07b9edc218a5
burçak kasap <burcak_kasap92@hotmail.com>
2022-09-22 16:52:06 +0300

    2. Hafta Ödevi

---
+++ classesWithAttributes/src/classesWithAttributes/SetIdParameter.java
@@ -0,0 +1,17 @@
+package classesWithAttributes;
+
+public class SetIdParameter {
+	private int id;
+
+	public SetIdParameter(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
